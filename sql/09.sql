25fe477c83afc253594f149ba9d950caf3ec9670
jalenjaloney <jalendeloney@gmail.com>
2026-03-05 23:52:31 -0800

    completed

--- sql/09.sql
+++ sql/09.sql
@@ -2,3 +2,18 @@
  * Use a JOIN to display the total amount rung up by each staff member in January of 2020.
  * Use tables staff and payment.
  */
+
+SELECT
+    s.first_name,
+    s.last_name,
+    SUM(p.amount) AS "sum"
+FROM
+    staff s
+JOIN
+    payment p ON s.staff_id = p.staff_id
+WHERE
+    EXTRACT(MONTH FROM p.payment_date) = 1
+    AND EXTRACT(YEAR FROM p.payment_date) = 2020
+GROUP BY
+    s.first_name,
+    s.last_name;
--- sql/16.sql
+++ sql/16.sql
@@ -4,3 +4,15 @@
  * Use tables payment, rental, inventory, and film. 
  */
 
+SELECT
+    f.title,
+    SUM(p.amount) AS profit
+FROM payment p
+JOIN rental r
+    ON p.rental_id = r.rental_id
+JOIN inventory i
+    ON r.inventory_id = i.inventory_id
+JOIN film f
+    ON i.film_id = f.film_id
+GROUP BY f.title
+ORDER BY profit DESC;
